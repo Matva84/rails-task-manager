08900677e6632c853bafb93bf0ef9b56080c0cc0
Mathieu VALLEAU <mathieuvalleau@hotmail.fr>
2024-01-29 20:18:42 +0100

    Terminé sauf options

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get "index" => "tasks#index", as: :index
+  get "new" => "tasks#new"
+  get ":id" => "tasks#show", as: :task
+  post "tasks" => "tasks#create"
+  patch ":id" => "tasks#update"
+  delete ":id" => "tasks#destroy"
   # Defines the root path route ("/")
-  # root "posts#index"
+  #root "posts#index"
 end
